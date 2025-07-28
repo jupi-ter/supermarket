26f874cd791759c31c55e2c0fcbddc508a569363
jules <102880647+jupi-ter@users.noreply.github.com>
2025-07-27 23:26:37 -0300

    shopping cart lol

---
+++ Assets/ShoppingCart.cs
@@ -0,0 +1,11 @@
+using Unity.Netcode;
+using UnityEngine;
+
+//implementar colliders reales y eso es un feature aparte
+//por ahora es un collider para todo, colisiones y deteccion.
+//pero deberian estar separados.
+
+public class ShoppingCart : NetworkBehaviour
+{
+
+}
